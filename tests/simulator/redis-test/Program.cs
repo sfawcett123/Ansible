134cef26b462066d3774721b7a453bc6518d1b6e
Steven Fawcett <64777084+sfawcett123@users.noreply.github.com>
2025-06-04 21:48:34 +0100

    dotnet test

---
+++ tests/simulator/redis-test/Program.cs
@@ -0,0 +1,8 @@
+﻿using NRedisStack;
+using StackExchange.Redis;
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("controller.local");
+IDatabase db = redis.GetDatabase();
+
+db.StringSet("foo", "Test Data");
+Console.WriteLine(db.StringGet("foo")); 
