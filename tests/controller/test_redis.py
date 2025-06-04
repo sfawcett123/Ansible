7e8926e739381729d36c91b36e04eb4338e46abb
Steven Fawcett <64777084+sfawcett123@users.noreply.github.com>
2025-06-04 21:13:22 +0100

    Test simple REDIS calls

--- tests/controller/test_redis.py
+++ tests/controller/test_redis.py
@@ -10,9 +10,27 @@ def port():
     return 6379
 
 @pytest.fixture
+def message():
+    return "bar"
+
+@pytest.fixture
+def channel():
+    return "foo"
+
+@pytest.fixture
 def connect( server , port ):
     return redis.Redis(host=server, port=port, decode_responses=True)
 
 def test_connection( connect ):
 #    with pytest.raises(Exception) as e_info:
          connect.ping() 
+
+def test_write( connect , channel, message  ):
+    connect.set( channel , message )
+
+def test_read( connect , channel, message ):
+    bar = connect.get( channel )
+    assert bar ==  message
+
+def test_destroy( connect , channel ):
+    connect.delete( channel )
